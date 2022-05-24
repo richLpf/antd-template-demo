0fb4a5f380ac850c039796540a404bda714d4985
richLpf <lpengfei66@163.com>
2022-05-24 18:53:17 +0800

    feat: action

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import "./index.less";
 
 ReactDOM.render(
   <Provider store={store}>
